980a5a24e3fd9fd4cefc8c601752f56acafbc14b
JEChoi <JEChoi@DESKTOP-RVSD0E8>
2020-09-11 01:49:08 +0900

    급여관리 - 총 근무시간, 총 근무일수 조회

--- AM/src/main/java/com/kh/am/pay/controller/PayController.java
+++ AM/src/main/java/com/kh/am/pay/controller/PayController.java
@@ -1,11 +1,27 @@
 package com.kh.am.pay.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.kh.am.calendar.model.vo.WorkCalendar;
+import com.kh.am.member.model.vo.Member;
+import com.kh.am.pay.model.service.PayService;
+
+@SessionAttributes({"loginMember", "loginEmployer"})
 @Controller
 @RequestMapping("/pay/*")
 public class PayController {
+	
+	@Autowired
+	private PayService payService;
 	
 	// 급여
 	
@@ -27,6 +43,49 @@ public class PayController {
 	public String updatePayStubView() {
 		return "pay/requestPayStub";
 	}
+	
+	// 알바생 조회하기
+	@ResponseBody
+	@RequestMapping("selectEmployeeList/{storeNo}")
+	public String selectEmployeeList(@PathVariable int storeNo) {
+		System.out.println("selecEmployee - 가게 번호: " + storeNo);
+		
+		List<Member> eList = payService.selectEmployeeList(storeNo);
+		System.out.println(eList);
+		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+		
+		return gson.toJson(eList);
+	}
+	
+	// 알바생 이번달 총 근무일수, 총 근무시간 조회
+	@ResponseBody
+	@RequestMapping("selectEmployeeWork")
+	public String selectEmployeeWork(int memberNo) {
+		
+		WorkCalendar wc = payService.selectEmployeeWork(memberNo);
+		
+		System.out.println("WC : " + wc);
+		
+		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+		
+		return gson.toJson(wc);
+	}
+	
+	/*@ResponseBody
+	@RequestMapping("selectOneDay")
+	public String selectOneDay(int memberNo) {
+		
+		int result = payService.selectOneDay(memberNo);
+		
+		System.out.println(result);
+		
+		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+		
+		return gson.toJson(result);
+	}
+	*/
+		
+		
 	
 	
 }
