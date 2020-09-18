c6b290995a03bcc7ed344848726a4f9d06fc9e1e
wangkobong <61865579+wangkobong@users.noreply.github.com>
2020-09-18 16:41:04 +0900

    0918 메일기능...

--- AM/src/main/java/com/kh/am/personnel/controller/PersonnelController.java
+++ AM/src/main/java/com/kh/am/personnel/controller/PersonnelController.java
@@ -125,7 +125,8 @@ public class PersonnelController {
 			
 		return "redirect:" + url;
 	}
-	  @RequestMapping(value = "/register", method = RequestMethod.POST)
+	
+	  /*@RequestMapping(value = "/register", method = RequestMethod.POST)
 	    public String RegisterPost(MemberVO user,Model model,RedirectAttributes rttr) throws Exception{
 	    
 	        System.out.println("regesterPost 진입 ");
@@ -133,7 +134,7 @@ public class PersonnelController {
 	        rttr.addFlashAttribute("msg" , "가입시 사용한 이메일로 인증해주세요");
 	        return "redirect:/";
 	    }
-
+	  
 	    //이메일 인증 코드 검증
 	    @RequestMapping(value = "/emailConfirm", method = RequestMethod.GET)
 	    public String emailConfirm(MemberVO user,Model model,RedirectAttributes rttr) throws Exception { 
@@ -148,5 +149,5 @@ public class PersonnelController {
 	        //System.out.println("usercontroller vo =" +vo);
 	        model.addAttribute("login",vo);
 	        return "/user/emailConfirm";
-	    }
+	    }*/
 }
